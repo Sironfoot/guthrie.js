dae6468b2f4b740ce170499d20b11f8c71c7b1a4
Dominic Pettifer <sironfoot@gmail.com>
2013-07-11 22:41:11 +0100

    Added some simple test cases to ecommerce app

--- examples/ecommerce/controllers/baseController.js
+++ examples/ecommerce/controllers/baseController.js
@@ -3,7 +3,8 @@ var gu = require('../..');
 var baseController = new gu.Controller();
 
 baseController.on('actionExecuting', function(req, res, next) {
-	next();
+    res.write('In baseController.actionExecuting');
+    next();
 });
 
 module.exports = baseController;
--- examples/ecommerce/controllers/homeController.js
+++ examples/ecommerce/controllers/homeController.js
@@ -1,31 +1,46 @@
 var gu = require('../..');
 
-var homeController = new gu.Controller();
+var homeController = new gu.Controller({
+    filters: [
+        function filter1(req, res, next) {
+            res.write('In homeController.filter1\n');
+            next();
+        },
+        function filter2(req, res, next) {
+            res.write('In homeController.filter2\n');
+            next();
+        }
+    ]
+});
 
 homeController.on('actionExecuting', function(req, res, next) {
-	next();
+    res.write('In homeController.actionExecuting\n');
+    next();
 });
 
 homeController.on('actionExecuted', function(req, res, next) {
-	next();
+    res.write('In homeController.actionExecuted\n');
+    next();
 });
 
 // Prob not possible
 homeController.on('resultExecuting', function(req, res, next) {
-	next();
+    res.write('In homeController.resultExecuting');
+    next();
 });
 
 // Prob not possible
 homeController.on('resultExecuted', function(req, res, next) {
-	next();
+    res.write('In homeController.resultExecuted');
+    next();
 });
 
 // PATH: /
 homeController.index = {
-	
-	GET: function(req, res) {
-		res.view();
-	}
+
+    GET: function(req, res) {
+        res.end('In homeController.index.GET');
+    }
 };
 
 module.exports = homeController;
