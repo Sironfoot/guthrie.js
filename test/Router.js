d075407c3cad1959bc47c76158c4cfb7b0233f92
Dominic Pettifer <sironfoot@gmail.com>
2013-07-24 22:02:20 +0100

    Added unit tests for Router.js

--- test/Router.js
+++ test/Router.js
@@ -1,13 +1,22 @@
 'use strict';
 
 var assert = require('assert');
+var path = require('path');
 var gu = require('../');
 
-/*
-describe('Router', function() {
-    it('should execute filters, actions and events in the correct order', function(done) {
 
+describe('Router', function() {
+    it('should set properties on express app instance', function(done) {
+
+        var app = {};
+        var rootDir = __dirname;
+        var controllersDir = path.join(rootDir, 'controllers');
+        var viewsDir = path.join(rootDir, 'views');
         
+        var router = new gu.Router(app, rootDir);
+        
+        assert.ok(app.gu, '.gu property is missing');
+        assert.equal(app.gu.controllersDir, controllersDir, '.controllersDir property is missing or incorrect');
+        assert.equal(app.gu.viewsDir, viewsDir, '.viewsDir property is missing or incorrect');
     });
 });
-*/
