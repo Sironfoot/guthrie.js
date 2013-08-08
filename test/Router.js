326ad4ed92cec477e7cf2c2eef7d49cd381a5f53
Dominic Pettifer <sironfoot@gmail.com>
2013-08-08 22:44:00 +0100

    Support for Areas

--- test/Router.js
+++ test/Router.js
@@ -7,7 +7,6 @@ var gu = require('../');
 
 describe('Router', function() {
     it('should set properties on express app instance', function() {
-
         var app = {};
         app.get = function() {
             return 'html';
@@ -20,7 +19,34 @@ describe('Router', function() {
         var router = new gu.Router(app, rootDir);
         
         assert.ok(app.gu, '.gu property is missing');
-        assert.equal(app.gu.controllersDir, controllersDir, '.controllersDir property is missing or incorrect');
-        assert.equal(app.gu.viewsDir, viewsDir, '.viewsDir property is missing or incorrect');
+        assert.equal(app.gu.rootDir, rootDir, '.rootDir is missing or incorrect')
+        assert.equal(app.gu.controllersDir, controllersDir, '.controllersDir is missing or incorrect');
+        assert.equal(app.gu.viewsDir, viewsDir, '.viewsDir is missing or incorrect');
+    });
+    
+    describe('#createArea', function() {
+        it('should set areas collection on express app instance', function() {
+            var app = {};
+            app.get = function() {
+                return 'html';
+            }
+            
+            var rootDir = __dirname;
+        
+            var areaName = 'admin';
+            var areaDir = path.join(rootDir, 'areas', areaName)
+            var controllersDir = path.join(areaDir, 'controllers');
+            var viewsDir = path.join(areaDir, 'views');
+        
+            var router = new gu.Router(app, rootDir);
+            var area = router.createArea(areaName);
+            
+            var areaInfo = app.gu.areas[areaName];
+            
+            assert.ok(areaInfo);
+            assert.equal(areaInfo.dir, areaDir);
+            assert.equal(areaInfo.controllersDir, controllersDir);
+            assert.equal(areaInfo.viewsDir, viewsDir);
+        });
     });
 });
